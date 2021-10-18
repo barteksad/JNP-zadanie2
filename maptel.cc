d8663049e1fe30de53b71beff9e9252fa1ef697a
barteksad <barteksadlej@gmail.com>
2021-10-18 18:09:29 +0200

    .

---
+++ maptel.cc
@@ -0,0 +1,8 @@
+#include "maptel.h"
+#include <string>
+
+
+int jnp::add(int x, int y) {
+    std::string s = "123";
+    return 1;
+}
---
+++ maptel.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+    extern "C" {
+    namespace jnp {
+#endif
+
+int add(int x, int y);
+
+
+#ifdef __cplusplus
+    }
+}
+#endif
